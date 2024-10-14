0aa5e438dff1c00e29a892d9f411400c8ac9a9b9
Brandon Hamilton <bham2015@gmail.com>
2024-10-14 14:27:40 -0500

    Imported router into the server.js file.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/users", require("./api/users"));
+
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
 });
