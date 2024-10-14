509fd5cab7747a5ea257315cfcb75d6c463d7684
Brandon Hamilton <bham2015@gmail.com>
2024-10-14 14:47:02 -0500

    Fixed a typo in the Router in the users.js file and successfully tested the GET /users endpoint.

--- api/users.js
+++ api/users.js
@@ -1,3 +1,14 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 module.exports = router;
+
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
